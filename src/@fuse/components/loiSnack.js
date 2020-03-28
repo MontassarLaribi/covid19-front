de0094812949f6b02e7f112802003f811d6e1be3
craxrev <mortadha.ghanmi56@gmail.com>
2020-03-28 16:56:41 +0100

    add translation for loi snack popup

--- src/@fuse/components/loiSnack.js
+++ src/@fuse/components/loiSnack.js
@@ -1,19 +1,24 @@
 import { Button, Snackbar } from "@material-ui/core";
 import * as React from "react";
+import { useTranslation } from "react-i18next";
+
 export default LoiSnack => {
+
+  const { t } = useTranslation("welcome");
+
   const [open, setOpen] = React.useState(true);
-  const message =
-    'امام التفشي السريع لوباء "فيروس كورونا المستجد" في تونس و في العالم، اصدرامرا حكوميا عدد 152 بتاريخ 13 مارس 2020 يصنف الوباء المذكور ضمن قائمة الامراض السارية عملا بالقانون عدد 71/92 بتاريخ 27 جويلية 1992. تهدف هذه المنصة الى دعم المجهودات المبذولة لمحاربة هذه الجائحة و هي على تواصل مع مصالح وزارتي الداخلية و الصحة العمومية. كل شخص او جيهة تحاول استعمالها في ما لا يجوز يعرض نفسه لتتبعات جزائية و عقوبات تصل الى السجن و ذلك عملا بأحكام المجلة الجزائية والقانون عدد 71/92 بتاجيخ 27 جويلية 1992 المتعلق بالامراض السارية المعدّل بالمرسوم عدد 152/2020 بتاريخ 13 مارس 2020.\n\r' +
-    "Face à la propagation rapide du covid-19 en Tunisie et dans le monde, le gouvernement a décidé, par le décret n°152 du 13 mars 2020, d’ajouter ce virus à la liste des maladies transmissibles citée par la loi n°71-1992 du 27 juillet 1992. Cette plateforme vise à porter soutien à la lutte contre la pandémie du covid-19 et est en lien direct avec les ministères de l’Intérieur et de la Santé Publique. Encombrer cette plateforme équivaut à commettre un délit pénal. Toute personne qui tente d’en détourner l’usage s’expose à des poursuites pénales et des peines d’emprisonnement conformément aux dispositions du Code pénal et de la Loi n°71-92 du 27 juillet 1992 relative aux maladies transmissibles, telle que modifiée par la Décret n°152-2020 du 13 mars 2020.";
+  const message = t("LOI_SNACK_TEXT");
   const action = (
     <Button onClick={handleCloseMessage} color="secondary" size="small">
-      Fermer
+      {t('LOI_SNACK_CLOSE_TEXT')}
     </Button>
   );
 
   function handleCloseMessage() {
     setOpen(false);
   }
+
+
 
   return (
     <Snackbar
--- src/app/page/welcome/i18n/fr.js
+++ src/app/page/welcome/i18n/fr.js
@@ -14,7 +14,9 @@ const locale = {
   ESPACE_SHOC_ROOM: "Espace Shoc Room",
   TEXT_WELCOME:
     "Mabaadhna est une platforme permettant la mise en relation rapide \ndes porteurs des symptômes du COVID-19 et l’accélération de leur prise en charge par le corps médical.",
-  TEXT_24H: "À votre service 24h/24 7j/7."
+  TEXT_24H: "À votre service 24h/24 7j/7.",
+  LOI_SNACK_TEXT: "Face à la propagation rapide du covid-19 en Tunisie et dans le monde, le gouvernement a décidé, par le décret n°152 du 13 mars 2020, d’ajouter ce virus à la liste des maladies transmissibles citée par la loi n°71-1992 du 27 juillet 1992. Cette plateforme vise à porter soutien à la lutte contre la pandémie du covid-19 et est en lien direct avec les ministères de l’Intérieur et de la Santé Publique. Encombrer cette plateforme équivaut à commettre un délit pénal. Toute personne qui tente d’en détourner l’usage s’expose à des poursuites pénales et des peines d’emprisonnement conformément aux dispositions du Code pénal et de la Loi n°71-92 du 27 juillet 1992 relative aux maladies transmissibles, telle que modifiée par la Décret n°152-2020 du 13 mars 2020.",
+  LOI_SNACK_CLOSE_TEXT: "Fermer"
 };
 
 export default locale;
