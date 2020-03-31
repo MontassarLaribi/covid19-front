c770d1972ff085d7059a9a2aa7aecc8fc9adffb8
Montazio <30464013+Montazio@users.noreply.github.com>
2020-03-31 15:19:40 +0200

    Build with social and contact

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.43f9e7f92ea50940e8fe6f4e745b7f7d.js"
+  "/precache-manifest.abea00ad95d2f91d07683af30f796ee8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
